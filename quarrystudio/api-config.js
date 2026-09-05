d49a417ef25bc5d378d371fc32a8b2322d17bbf1
Muzzamil hassan <129590453+muzzamilhassan@users.noreply.github.com>
2026-09-06 00:39:42 +0500

    api base localhost

--- quarrystudio/api-config.js
+++ quarrystudio/api-config.js
@@ -1,3 +1,3 @@
-window.QS_API_BASE = "SET_AFTER_TUNNEL";
+window.QS_API_BASE = "http://localhost:8787";
 window.QS_CLIENT_KEY = "SET_AFTER_APP_CREATION";
 window.QS_REDIRECT = "https://muzzamilhassan.github.io/quarrystudio/callback.html";
